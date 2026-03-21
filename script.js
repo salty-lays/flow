cb44ba03539318043ee5942189bff2320064d132
=salty-lays <=chetanpatil21444@gmail.com>
2026-03-21 21:35:31 +0530

    pushed

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
-function startTroll() {
-  document.getElementById("startBtn").style.display = "none";
-  
-  const screen = document.getElementById("screen");
-  const bar = document.getElementById("progressBar");
-  
-  screen.style.display = "block";
-  screen.classList.add("glitch");
-  
-  const messages = [
-    "Initializing system breach...",
-    "Bypassing firewall...",
-    "Injecting payload...",
-    "Accessing private files...",
-    "Downloading data...",
-    "Tracking IP address...",
-    "Location found: Mumbai, India",
-    "Sending data to server...",
-    "Granting remote access...",
-    "ERROR... ERROR...",
-    "System override complete."
-  ];
-  
-  let i = 0;
-  let progress = 0;
-  
-  const textInterval = setInterval(() => {
-    if (i < messages.length) {
-      screen.innerText += messages[i] + "\n";
-      i++;
-    } else {
-      clearInterval(textInterval);
-      finalChaos();
-    }
-  }, 700);
-  
-  const progressInterval = setInterval(() => {
-    progress += Math.random() * 10;
-    bar.style.width = progress + "%";
-    
-    if (progress >= 100) {
-      clearInterval(progressInterval);
-    }
-  }, 300);
-  
-  function finalChaos() {
-    setInterval(() => {
-      document.body.style.background =
-        document.body.style.background === "black" ? "#100" : "black";
-      
-      screen.style.color =
-        screen.style.color === "red" ? "#00ff9f" : "red";
-      
-      screen.innerText += "\n⚠️ SYSTEM COMPROMISED ⚠️";
-    }, 200);
-  }
-}
+let hint = document.getElementById("hint");
+
+hint.addEventListener("click",()=>{
+  hint.innerText="z-index in css";
+  hint.style.height ="70px";
+  hint.style.width ="300px";
+  hint.style.borderRadius = "1px";
+  hint.style.margin = "5vh 0 0 35vw"
+})
