19a8ff4a0f393136e01b3819d0b85cc7f6c29403
=salty-lays <=chetanpatil21444@gmail.com>
2026-03-30 20:21:03 +0530

    kk

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-let heading = document.querySelector('h1');
-let inputColor = document.getElementById('color');
-let inputIndex = document.getElementById('index');
-let text = heading.innerText;
+let in1 = document.querySelector('#fstIn');
+let in2 = document.querySelector('#secIn');
+let operator = document.querySelector('#operator');
+let result = document.getElementById('res');
 
-heading.innerHTML = text
- .split("")
- .map(letter => `<span>${letter}</span>`)
- .join("");
+function calculate(valOne, valTwo, valOperator) {
+  let ans;
+  
+  switch (valOperator) {
+    case '+':
+      ans = valOne + valTwo;
+      break;
+    case '-':
+      ans = valOne - valTwo;
+      break;
+    case '*':
+      ans = valOne * valTwo;
+      break;
+    case '/':
+      if (valTwo === 0) return ""; // prevent divide by zero
+      ans = valOne / valTwo;
+      break;
+    default:
+      return "";
+  }
+  
+  return ans;
+}
 
-const changeColor = () => {
- let color = inputColor.value;
- let index = Number(inputIndex.value) - 1;
- try {
-  document.getElementById('error').innerText = ''
- let letters = heading.querySelectorAll("span");
- letters[index].style.color = color;
+function updateResult() {
+  let val1 = parseFloat(in1.value);
+  let val2 = parseFloat(in2.value);
+  let op = operator.value;
+  
+  // check valid inputs
+  if (isNaN(val1) || isNaN(val2)) {
+    result.innerText = "";
+    return;
+  }
+  
+  let ans = calculate(val1, val2, op);
+  
+  // only show valid result
+  result.innerText = ans !== undefined ? ans : "";
 }
-catch {
- document.getElementById('error').innerText = 'PLEASE enter Valid Values';
-}
-}
+
+// Real-time listeners
+in1.addEventListener("input", updateResult);
+in2.addEventListener("input", updateResult);
+operator.addEventListener("change", updateResult);
