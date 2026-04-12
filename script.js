9ea75af0dfaa50a20932173a75f3d39eb99e2d90
=salty-lays <=chetanpatil21444@gmail.com>
2026-04-12 22:02:59 +0530

    touch move

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-import { getAnswer } from './checker.js';
+const character = document.getElementById("character");
+const cont = document.querySelector(".cont");
+const reward = document.getElementById('reward');
 
-let body = document.querySelector('body');
-let btn = document.querySelector('button');
-let ans = document.querySelector('.ans');
-let quest = document.getElementById('question');
-let hints = ["how are you", "love", "time", "date",'day','age','name'];
-let hintIndex = 0;
+let isDragging = false;
 
+character.addEventListener("pointerdown", () => {
+  isDragging = true;
+});
 
-btn.addEventListener('click',()=>{
-  var asked = quest.value.toLowerCase().trim();
-  clearInterval(hintInterval);
-  if (!asked) {
-    quest.placeholder = "Please type something!";
-    return;
+document.addEventListener("pointerup", () => {
+  isDragging = false;
+});
+
+document.addEventListener("pointermove", (e) => {
+  if (isDragging) {
+    const rect = cont.getBoundingClientRect();
+    
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    
+    const maxX = rect.width - character.offsetWidth;
+    const maxY = rect.height - character.offsetHeight;
+    
+    if (x < 0) x = 0;
+    if (y < 0) y = 0;
+    if (x > maxX) x = maxX;
+    if (y > maxY) y = maxY;
+    
+    if (x == maxX && y == maxY) {
+     const text = document.querySelector('h2'); 
+      text.innerText = 'YOU WON';
+      setTimeout(()=>{
+    character.style.left = 0 +"px";
+    character.style.top = 0 + "px";
+    text.innerText = '';
+      },2000)
+    }
+    character.style.left = x + "px";
+    character.style.top = y + "px";
+    
   }
-  ans.innerText = getAnswer(asked);
-})
-
-function showHints() {
-  quest.placeholder = "Try asking: " + hints[hintIndex];
-  hintIndex = (hintIndex + 1) % hints.length;
-}
-let hintInterval = setInterval(showHints, 1200);
+});
